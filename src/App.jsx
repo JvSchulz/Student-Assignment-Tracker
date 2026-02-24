5cb715fbbe63f336ecab8d9ebfabf15ccf9929da
JvSchulz <vitorsm66@hotmail.com>
2026-02-24 16:12:12 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmployeeDirectory from './EmployeeDirectory';
+
+function App() {
+  return (
+    <div>
+      <EmployeeDirectory />
+    </div>
+  );
+}
+
+export default App;
